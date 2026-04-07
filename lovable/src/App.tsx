a074b55be3e7d7a55104cacb7f3ee6f4df6c884e
FranciscoGonzalez03 <frangonzalez@frba.utn.edu.ar>
2026-04-07 20:26:21 -0300

    Sync latest lovable changes to docs and medical

--- lovable/src/App.tsx
+++ lovable/src/App.tsx
@@ -1,9 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AppProvider } from "@/context/AppContext";
 import Index from "./pages/Index.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +15,12 @@ const App = () => (
       <AppProvider>
         <Toaster />
         <Sonner />
-        <Index />
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </HashRouter>
       </AppProvider>
     </TooltipProvider>
   </QueryClientProvider>
